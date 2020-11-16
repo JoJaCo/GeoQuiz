a22284c861f6023f811e156dee7e5b6543b09a0c
JoJaCo <contrerasjorge226@gmail.com>
2020-11-16 05:15:40 -0800

    added a backbtn with code

--- app/src/main/java/com/bignerdramch/andriod/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdramch/andriod/geoquiz/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
+    private lateinit var prevButton: Button
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -32,6 +33,7 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        prevButton = findViewById(R.id.pre_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener{View ->
@@ -50,7 +52,20 @@ class MainActivity : AppCompatActivity() {
             updateQuestion()
         }
         updateQuestion()
+
+        prevButton.setOnClickListener {
+            if (currentIndex != 0) {
+                currentIndex = (currentIndex - 1) % questionBank.size
+                updateQuestion()
+            }else
+                Toast.makeText(this,
+                    "this is the first question",
+                    Toast.LENGTH_SHORT).show()
+
+        }
     }
+
+
 
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
